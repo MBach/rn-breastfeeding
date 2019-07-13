df25e34c282efb768bf0de9ba2d7a4071176cb06
Matthieu BACHELIER <bachelier.matthieu@gmail.com>
2019-07-13 15:34:34 +0200

    Update to RN 0.60

--- android/app/src/main/java/io/matierenoire/breastfeeding/MainApplication.java
+++ android/app/src/main/java/io/matierenoire/breastfeeding/MainApplication.java
@@ -2,24 +2,17 @@ package io.matierenoire.breastfeeding;
 
 import android.app.Application;
 
+import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import com.heanoria.library.reactnative.locationenabler.RNAndroidLocationEnablerPackage;
-import com.oblador.vectoricons.VectorIconsPackage;
-import com.reactNativeQuickActions.AppShortcutsPackage;
-import com.reactcommunity.rnlocalize.RNLocalizePackage;
-import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
-import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 
-import java.util.Arrays;
 import java.util.List;
 
 /**
  * @author Matthieu BACHELIER
- * @version 1.0
+ * @version 1.1
  * @since 2019-02
  */
 public class MainApplication extends Application implements ReactApplication {
@@ -32,16 +25,11 @@ public class MainApplication extends Application implements ReactApplication {
 
         @Override
         protected List<ReactPackage> getPackages() {
-            return Arrays.asList(
-                    new MainReactPackage(),
-                    new RNLocalizePackage(),
-                    new AsyncStoragePackage(),
-                    new RNAndroidLocationEnablerPackage(),
-                    new AppShortcutsPackage(),
-                    new RNGestureHandlerPackage(),
-                    new RNBreastFeedingPackage(),
-                    new VectorIconsPackage()
-            );
+            @SuppressWarnings("UnnecessaryLocalVariable")
+            List<ReactPackage> packages = new PackageList(this).getPackages();
+            // Packages that cannot be autolinked yet can be added manually here, for example:
+            // packages.add(new MyReactNativePackage());
+            return packages;
         }
 
         @Override
--- android/app/src/main/java/io/matierenoire/breastfeeding/RNBreastFeedingModule.java
+++ android/app/src/main/java/io/matierenoire/breastfeeding/RNBreastFeedingModule.java
@@ -9,9 +9,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
-import android.support.v4.app.NotificationCompat;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
 import android.util.Log;
 
 import com.facebook.react.bridge.Arguments;
