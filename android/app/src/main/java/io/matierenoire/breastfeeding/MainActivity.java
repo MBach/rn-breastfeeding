aec32e321e35cefc6a1f3af22c0ffb1952b8299c
Matthieu BACHELIER <bachelier.matthieu@gmail.com>
2019-07-15 00:25:31 +0200

    Update to RN 0.60

--- android/app/src/main/java/io/matierenoire/breastfeeding/MainActivity.java
+++ android/app/src/main/java/io/matierenoire/breastfeeding/MainActivity.java
@@ -1,10 +1,13 @@
 package io.matierenoire.breastfeeding;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 /**
  * @author Matthieu BACHELIER
- * @version 1.0
+ * @version 1.1
  * @since 2019-02
  */
 public class MainActivity extends ReactActivity {
@@ -12,5 +15,15 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "BreastFeeding";
+    }
+
+    @Override
+    protected ReactActivityDelegate createReactActivityDelegate() {
+        return new ReactActivityDelegate(this, getMainComponentName()) {
+            @Override
+            protected ReactRootView createRootView() {
+                return new RNGestureHandlerEnabledRootView(MainActivity.this);
+            }
+        };
     }
 }
